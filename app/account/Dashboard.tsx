1612f7343ab31a054c72dfd98c1202d9ead57d34
Alison Day <alisonday@Alisons-MacBook-Pro-4.local>
2023-07-21 23:31:01 -0600

    Worked on dashboard

--- app/account/Dashboard.tsx
+++ app/account/Dashboard.tsx
@@ -2,6 +2,7 @@
 import { useCallback, useEffect, useState } from 'react'
 import { Database } from '../database.types'
 import { Session, createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import Chatbot from '@/components/Chatbot'
 
 export default function Dashboard({ session }: { session: Session | null }) {
   const supabase = createClientComponentClient<Database>()
@@ -38,7 +39,16 @@ export default function Dashboard({ session }: { session: Session | null }) {
 
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <div className='relative w-[500px] h-fit min-h-[500px] bg-white rounded-xl shadow flex flex-col py-[25px] gap-8'>
+      <div className='relative w-[1000px] h-fit min-h-[500px] bg-white rounded-xl shadow flex flex-col gap-8 px-[50px] py-[10px] items-center'>
+        <h1 className='relative w-full font-semibold text-2xl text-center'>Dashboard</h1> 
+        <div className='relative w-full flex flex-col gap-2'>
+          <h1> Paste resume or any other relevant information</h1>
+          <textarea placeholder="Paste Information Here" className='w-full h-[100px] py-[5px] px-[5px] outline-none border-gray-600 border-2 rounded-md'/>
+        </div>
+        <div className='relative flex flex-col gap-2 w-full py-[10px]'>
+          <h1>Chatbot Preview</h1>
+            <Chatbot/>
+        </div>
       </div>
     </div>
   )
---
+++ components/Chatbot.tsx
@@ -0,0 +1,7 @@
+export default function Chatbot(){
+    return(
+        <div className="relative w-fit gap-2 h-[50px] flex border-2 border-gray-600 rounded-md overflow-hidden">
+            <input type='text' className=""/>
+        </div>
+    )
+}
