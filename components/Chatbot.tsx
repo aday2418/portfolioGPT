eb1f20e39582f1742b276e5ba156a25f6d8b3058
Alison Day <alisonday@Alisons-MacBook-Pro-4.local>
2023-07-22 23:44:11 -0600

    Saved Message State

--- components/Chatbot.tsx
+++ components/Chatbot.tsx
@@ -1,15 +1,31 @@
+'use client'
+
+import React, { ChangeEvent, FormEventHandler, useState } from "react";
 import ChatHistory from "./ChatHistory";
 
 export default function Chatbot(){
+    const [messageHistory, setMessageHistory] = useState<string[]>([])
+    const [currentMessage, setCurrentMessage] = useState<string>('')
+
+    const changeMessage = (e: ChangeEvent<HTMLInputElement>) => {
+        setCurrentMessage(e.target.value)
+    }
+
+    const addMessage = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        setMessageHistory(messageHistory.concat(currentMessage))
+        setCurrentMessage('')
+    }
+    
     return(
         <div className="relative w-[600px] bg-gray-200 rounded-md ">
             <ChatHistory/>
-            <div className="relative w-full gap-2 h-[50px] flex border border-gray-300 rounded-md overflow-hidden smoothe shadow-md hover:shadow-lg bg-white">
-                <input type='text' placeholder='Ask me anything...' className="w-full px-[10px] outline-none"/>
-                <button className="h-full bg-blue-400  text-white font-medium tracking-wider px-[10px] hover:bg-blue-500 smoothe ">
+            <form onSubmit={addMessage} className="relative w-full gap-2 h-[50px] flex border border-gray-300 rounded-md overflow-hidden smoothe shadow-md hover:shadow-lg bg-white">
+                <input value={currentMessage} onChange={changeMessage} type='text' placeholder='Ask me anything...' className="w-full px-[10px] outline-none"/>
+                <button type='submit' className="h-full bg-blue-400  text-white font-medium tracking-wider px-[10px] hover:bg-blue-500 smoothe ">
                     Submit
                 </button>
-            </div>
+            </form>
         </div>
     )
 }
